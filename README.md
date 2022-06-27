1b88c28e57350f250f0089f2c00ea60b46f97072
* <koutuk.vidyalay@gmail.com>
2022-06-27 15:24:29 +0530

    13-06

--- README.md
+++
@@ -1,2 +0,0 @@
-# acharya-technologies.github.io
-abc
---
+++ js/typewriter.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.typewriter',{ strings: [
+`I am  <b style='color:tomato;font-weight:900;'>चि.  ओंकार महेश कुलकर्णी</b>.
+A 16-years old boy studying "Computer Science and Engineering" in --- College.<br>
+I am a beginner in Web-development.<br>
+I am also a <span class="myEduC">Cyber-Security</span> aspirant hoping for more <b>"SECURE WORLD !"</b>.`],
+	typeSpeed: 80,contentType:'html',backSpeed:80 });
+
