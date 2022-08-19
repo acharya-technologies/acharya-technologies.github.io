50313a924c5584323029a596cca3943905ea89eb
* <kulkarni.ok.01@gmail.com>
2022-08-19 22:29:40 +0530

    श्री

---
+++ js/typewriter.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.typewriter',{ strings: [
+`I am  <b class="type-name">चि.  ओंकार महेश कुलकर्णी</b>.
+A 16-years old boy studying "Computer Science and Engineering" in --- College.<br>
+I am a beginner in Web-development.<br>
+I am also a <span class="myEduC">Cyber-Security</span> aspirant hoping for more <i class="myEduC">"SECURE  WORLD !"</i>.`],
+	typeSpeed: 50,contentType:'html',backSpeed:10 });
