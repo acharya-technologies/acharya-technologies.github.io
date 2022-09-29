811bbfaaf0bbb97fd3554e3c537ce6fe63c4d28a
Omkar Kulkarni <kulkarni.ok.01@gmail.com>
2022-09-29 22:08:46 +0530

    श्री

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function quote(){
 const all=[`Where there is <s>will</s> <i>CODE</i> ,<br>There is a <s>Way</s> <i>BUG</i>.`,
 	   `Programming =  Coffee × Code`,
 	   `It is not a <b>BUG</b> ,<br> It is a <i> FEATURE</i>.`,
-	   `There is no <button class='rounded border-2 border-black bg-gray-100 px-1'>Ctrl + Z</button> in <i>Life.</i>`,
+	   `There is no <button class='rounded border-2 border-black dark:bg-stone-500 bg-gray-100 px-1'>Ctrl + Z</button> in <i>Life.</i>`,
 	   `<kbd>10</kbd> type of people in the world.<br><kbd>1)</kbd>Who understand binary. <kbd>2)</kbd>Who don't.</li>`,
 	   `There is no place like <s>HOME</s> <k>127.0.0.1</k> .`,
 	   `<b class='text-red-500'>!</b> <code>false</code> . This is called <code>true</code>.`,
