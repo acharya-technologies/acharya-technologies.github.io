a5a485fb76813479b530066a4403383064395201
Omkar Kulkarni <kulkarni.ok.01@gmail.com>
2022-09-30 09:43:53 +0530

    श्री

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,6 +1,6 @@
 var typed = new Typed('.typewriter',{ strings: [
 `I am  <b class="type-name">चि.  ओंकार महेश कुलकर्णी</b>.
-A 16-years old boy studying "Computer Science and Engineering" in --- College.<br>
+A 16-years old boy studying "Diploma in <i>Computer Hardware and Maintance</i>" in RIT, Islampur College.<br>
 I am a beginner in Web-development.<br>
 I am also a <span class="myEduC">Cyber-Security</span> aspirant hoping for more <i class="myEduC">"SECURE  WORLD !"</i>.`],
 	typeSpeed: 50,contentType:'html',backSpeed:10 });
